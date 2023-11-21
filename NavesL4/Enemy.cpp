067adcd7be0c57fea91e5bc8e3e6502b0fd2d7e5
NoelEE <uo289307@uniovi.es>
2023-11-21 22:09:32 +0100

    cambios restaurados

--- NavesL4/Enemy.cpp
+++ NavesL4/Enemy.cpp
@@ -1,30 +1,20 @@
 #include "Enemy.h"
 
-Enemy::Enemy(string fileName, float x, float y, Game* game)
-	: Actor(fileName, x, y, 36, 40, game) {
-	vx = 0;
-	this->animation = new Animation(fileName); // insertar resto de parámetros para animación de combate
-
+Enemy::Enemy(float x, float y, Game* game)
+	: Actor("res/enemigo.png", x, y, 36, 40, game) {
 
 }
 
 void Enemy::update() {
 	// Actualizar la animación
 	animation->update();
-}
 
-void Enemy::switchProfile() {
-	battle = !battle;
+	vx = -1;
+	x = x + vx;
+
 }
 
 void Enemy::draw(float scrollX, float scrollY) {
-	if (battle)
-		animation->draw(x - scrollX, y - scrollY);
-	else
-		
-}
-
-void Enemy::damage(int received) {
-	this->health -= received;
+	animation->draw(x - scrollX, y - scrollY);
 }
 
--- NavesL4/Enemy.h
+++ NavesL4/Enemy.h
@@ -6,16 +6,9 @@
 class Enemy : public Actor
 {
 public:
-	Enemy(string filename, float x, float y, Game* game);
+	Enemy(float x, float y, Game* game);
 	void draw(float scrollX = 0, float scrollY = 0) override; // Va a sobrescribir
 	void update();
-	void damage(int received);
-	void switchProfile();
-	Animation* aBattle;
+	Animation* aMoving;
 	Animation* animation; // Referencia a la animación mostrada
-
-	int health;
-	string animationName;
-	string spriteName;
-	bool battle = false;
 };
